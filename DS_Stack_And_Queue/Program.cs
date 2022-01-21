6fb8b048d69c88a72ab0906c082ab09e92bcfed1
Lenovo <somusp75@gmail.com>
2022-01-21 18:16:39 +0530

    Uc3-Enqueue Method

--- DS_Stack_And_Queue/Program.cs
+++ DS_Stack_And_Queue/Program.cs
@@ -6,36 +6,53 @@ namespace DS_Stack_And_Queue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("----------------Creating Stack Operations-------------------");
-           
-            Console.WriteLine("     Enter the option     ");
-            Console.WriteLine("\n1.Push Operation \n2.Pop Operation");
-            int num = Convert.ToInt32(Console.ReadLine());
-            Stack stack = new Stack();
-            switch (num)
+            Console.WriteLine("----------------Creating Stack & Queue Operations-------------------");
+            Console.WriteLine("***************************************************************************");
+            string flag = "Y";
+            while (flag == "Y" || flag == "y")
             {
-                case 1:
-                    stack.Push(70);
-                    stack.Push(30);
-                    stack.Push(56);
-                    Console.WriteLine("==================================");
-                    stack.Display();
-                    break;
-                case 2:
-                    stack.Push(70);
-                    stack.Push(30);
-                    stack.Push(56);
-                    
-                    Console.WriteLine("----------------------------------Using Peek method");
-                    //Console.WriteLine("Using Peek method");
-                    stack.Pop();
-                    stack.Display();
-                    break;
-                default:
-                    Console.WriteLine("Enter a valid option");
-                    break;
+
+                Console.WriteLine("     Enter the option  1,2,3,4   ");
+                Console.WriteLine("-------Stack Operation----- \n1.Push Operation \n2.Pop and Peek Operation\n \n------Queue Operation------- \n3.EnQueue Operation\n");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Stack stack = new Stack();
+                switch (num)
+                {
+                    case 1:
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+                        Console.WriteLine("==================================");
+                        stack.Display();
+                        break;
+                    case 2:
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+
+                        Console.WriteLine("----------------------------------Using Peek method");
+                        //Console.WriteLine("Using Peek method");
+                        stack.Pop();
+                        stack.Display();
+                        break;
+                    case 3:
+                        Queue queue = new Queue();
+                        queue.Enqueue(56);
+                        queue.Enqueue(30);
+                        queue.Enqueue(70);
+                        Console.WriteLine("===============================");
+                        queue.Display();
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter a valid option");
+                        break;
+
+                }
+                Console.WriteLine("\nDo you want to continue?(Y/N)");
+                flag = Console.ReadLine();
             }
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
