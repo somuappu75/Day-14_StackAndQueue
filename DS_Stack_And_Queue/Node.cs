6f0bd8495c21b7fae4bbcf20d42ecf97465bb9ee
Lenovo <somusp75@gmail.com>
2022-01-21 17:12:57 +0530

    Uc-1 Stack created

---
+++ DS_Stack_And_Queue/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DS_Stack_And_Queue
+{
+    class Node
+    {
+         
+        public int data;
+        public Node next;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+}
