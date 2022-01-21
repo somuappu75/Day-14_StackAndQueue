433a4046ce69b0d940927fc3929c06168ad30107
Lenovo <somusp75@gmail.com>
2022-01-21 16:33:04 +0530

    Add project files.

---
+++ DS_Stack_And_Queue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DS_Stack_And_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
